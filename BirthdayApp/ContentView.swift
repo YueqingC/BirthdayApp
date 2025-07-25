f4cb1d96a5f0583f0c56cfc3bfcba46993782e6a
YueqingC <gongilh31@gmail.com>
2025-07-25 13:31:56 -0400

    1

--- BirthdayApp/ContentView.swift
+++ BirthdayApp/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var friends: [Friend] = [
+        Friend(name: "Joan", birthday: .now),
+        Friend(name: "Sienna", birthday: .now)
+    ]
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            List(friends, id: \.name){ friend in
+                HStack{
+                    Text(friend.name)
+                    Spacer()
+                    Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                }
+            }
+            .navigationBarTitle("Birthdays")
+            .safeAreaInset(edge: .bottom){
+                VStack(alignment: .center, spacing: 20){
+                    Text("New Birthday")
+                        .font(.headline)
+                    DatePicker(selection: $newBirthday){
+                        TextField("Name", text: $newName)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    Button("Save"){
+                        let newFriend = friend(name: newName, birthday: newBirthday)
+                        friends.append(newFriend)
+                    }
+                    .bold()
+                }
+                .padding()
+                .background(.bar)
+            }
         }
-        .padding()
     }
 }
 
---
+++ BirthdayApp/Friend.swift
@@ -0,0 +1,18 @@
+//
+//  Friend.swift
+//  BirthdayApp
+//
+//  Created by Yueqing Cai on 7/25/25.
+//
+
+import Foundation
+
+class friend{
+    var name: String
+    var birthday: Date
+    
+    init(name: String, birthday: Date){
+        self.name = name
+        self.birthday = birthday
+    }
+}
